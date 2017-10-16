06a46824f9b37fca6691c4e343c0bbe552dc05de
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-16 16:52:37 +0000

    implements character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('textarea').on('keyup', function(event) {
+    var charset = (140 - event.target.value.length);
+    var counter = $(event.target).parent().children('.counter');
+
+    counter.text(charset);
+
+    if (charset < 0) {
+      counter.addClass('negCharset');
+    } else {
+      counter.removeClass('negCharset');
+    }
+  });
+});
