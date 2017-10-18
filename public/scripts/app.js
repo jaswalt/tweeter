a735790e6ff3a44633a0014d5393928bb465a658
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-18 21:35:58 +0000

    get tweets using ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from tweets.json
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 const created = (date) => {
   let timeElapsed = Date.now() - date;
@@ -86,9 +39,9 @@ function createTweetElement(tweet) {
             <footer>
               ${ $('<div class="days">').text(created(tweet.created_at)).prop('outerHTML') }
               <div class="glyph-icons">
-                <i class="fa fa-flag" aria-hidden="true"></i>
-                <i class="fa fa-retweet" aria-hidden="true"></i>
-                <i class="fa fa-heart" aria-hidden="true"></i>
+                <i class="fa fa-flag hidden" aria-hidden="true"></i>
+                <i class="fa fa-retweet hidden" aria-hidden="true"></i>
+                <i class="fa fa-heart hidden" aria-hidden="true"></i>
               </div>
             </footer>
     </article>`
@@ -98,9 +51,8 @@ function createTweetElement(tweet) {
 
 
 $(document).ready(function() {
-  renderTweets(data);
-  var form = $('.new-tweet form');
 
+  var form = $('.new-tweet form');
   form.on('submit', function(event) {
     event.preventDefault();
 
@@ -111,5 +63,16 @@ $(document).ready(function() {
     });
   })
 
+  const loadTweets = () => {
+
+    $.ajax({
+      url: '/tweets',
+      type: 'GET',
+    })
+    .done(function(data) {
+      renderTweets(data);
+    });
+  }
+  loadTweets();
 });
 
