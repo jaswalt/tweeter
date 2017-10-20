6cf9ff8421b0042f60bea39cc7f5c2dc76b34999
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-20 21:00:18 +0000

    refactors created function and cleans up indentation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,42 @@
 const created = (date) => {
- let timeElapsed = Date.now() - date;
- if (timeElapsed < 1000000) {
-   return Math.floor(timeElapsed / 10000) + " minutes ago";
- } else {
-   return Math.floor(timeElapsed / 1000000000) + " days ago";
- }
+  let timeElapsed = Date.now() - date;
+  if (timeElapsed > 31622400000) {
+    return Math.floor(timeElapsed / 1000 / 60 / 60 / 24 / 365) + " years ago";
+  } else if (timeElapsed > 86400000) {
+    return Math.floor(timeElapsed / 1000 / 60 / 60 / 24) + " days ago";
+  } else if (timeElapsed > 3600000) {
+    return Math.floor(timeElapsed / 1000 / 60 / 60) + " hours ago";
+  } else if (timeElapsed > 60000) {
+    return Math.floor(timeElapsed / 1000 / 60) + " minutes ago";
+  } else {
+    return Math.floor(timeElapsed / 1000) + " seconds ago";
+  }
 }
 
 function createTweetElement(tweet) {
- //var $tweet = $('<article>').addClass('each-tweet');
+  let tweetHTML = `
+    <article class="each-tweet">
+      <header>
+        ${ $('<img class="image">').attr('src', tweet.user.avatars.regular).prop('outerHTML') }
+        ${ $('<span class="name">').text(tweet.user.name).prop('outerHTML') }
+        <span class="handle">${tweet.user.handle}</span>
+      </header>
+      ${ $('<p>').text(tweet.content.text).prop('outerHTML') }
+      <footer>
+        ${ $('<div class="days">').text(created(tweet.created_at)).prop('outerHTML') }
+        <div class="glyph-icons">
+          <i class="fa fa-flag hidden" aria-hidden="true"></i>
+          <i class="fa fa-retweet hidden" aria-hidden="true"></i>
+          <i class="fa fa-heart hidden" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </article>`
 
- let tweetHTML = `
-   <article class="each-tweet">
-           <header>
-             ${ $('<img class="image">').attr('src', tweet.user.avatars.regular).prop('outerHTML') }
-             ${ $('<span class="name">').text(tweet.user.name).prop('outerHTML') }
-             <span class="handle">${tweet.user.handle}</span>
-           </header>
-             ${ $('<p>').text(tweet.content.text).prop('outerHTML') }
-           <footer>
-             ${ $('<div class="days">').text(created(tweet.created_at)).prop('outerHTML') }
-             <div class="glyph-icons">
-               <i class="fa fa-flag hidden" aria-hidden="true"></i>
-               <i class="fa fa-retweet hidden" aria-hidden="true"></i>
-               <i class="fa fa-heart hidden" aria-hidden="true"></i>
-             </div>
-           </footer>
-   </article>`
-
- return tweetHTML;
+  return tweetHTML;
 }
 
 function renderTweets(tweets) {
- // loops through tweets
- // calls createTweetElement for each tweet
   tweets.forEach(function(tweet) {
-    // takes return value and prepends it to the tweets container
     tweetElement = createTweetElement(tweet);
     $('.tweets').prepend(tweetElement);
   });
@@ -52,21 +53,17 @@ const loadTweets = () => {
   });
 }
 
-
+// DOC.READY
 $(document).ready(function() {
 
-
-
-// Hide new-tweet
+  // Hide new-tweet
   $('.new-tweet').hide();
-
-// Compose button shows and hides new-tweet
+  // Compose button shows and hides new-tweet
   $('#compose').click(function() {
     $('.new-tweet').slideToggle();
   });
 
-
-  var form = $('.new-tweet form');
+  const form = $('.new-tweet form');
   form.on('submit', function(event) {
     event.preventDefault();
 
